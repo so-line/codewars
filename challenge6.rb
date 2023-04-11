100b1b753d4f72eb3c12361f65168b21cb7df508
soline dziadzio <soline.dziadzio@gmail.com>
2023-04-11 00:32:18 -0400

    2 more codewar challenges

---
+++ challenge6.rb
@@ -0,0 +1,14 @@
+#Basic Mathematical Operations
+
+def basic_op(operator, value1, value2)
+  case operator
+  when "+"
+    value1 + value2
+  when "-"
+    value1 - value2
+  when "*"
+    value1 * value2
+  when "/"
+    value1 / value2
+  end
+end
